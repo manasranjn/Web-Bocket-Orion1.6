741349b9c6ad4d1953d141fa4ce0930119cadeaf
Manas Ranjan Bera <manasbera484@gmail.com>
2025-06-30 16:53:05 +0530

    DOM

---
+++ JavaScript/Class 6/script.js
@@ -0,0 +1,53 @@
+let arr = ["Priyansu", "Sibasish", "Kaushik", "Agnisikha", "Soumya", "Mehreen"]
+
+// let newArr = arr.forEach((element) => {
+//     console.log(element);
+//     return element
+// })
+// console.log(newArr);
+
+// let newArr = arr.map((element, i) => {
+//     console.log(element);
+//     console.log(i);
+
+//     return "Hi " + element
+// })
+// console.log(newArr);
+
+let marks = [90, 80, 70, 75, 85, 74, 56, 65, 40, 48]
+// let newMarks = marks.map((item) => {
+//     if (item > 60) {
+//         return item
+//     }
+// })
+// console.log(newMarks);
+
+// let filteredMarks = marks.filter((item) => {
+//     return item > 60
+// })
+// // console.log(filteredMarks);
+
+// let sum = marks.reduce((sum, item) => sum += item, 0)
+// console.log(sum);
+
+// console.log(window);
+// console.log(document);
+
+// const heading = document.getElementById("heading")
+// console.log(heading);
+
+// const paragraph = document.getElementsByClassName("paragraph")
+// console.log(paragraph);
+
+// const head = document.getElementsByTagName("h1")
+// console.log(head);
+
+// let input = document.getElementsByName("email")
+// console.log(input);
+
+let headingEle = document.querySelector("h1")
+console.log(headingEle);
+
+
+let paragraphEle = document.querySelectorAll(".paragraph")
+console.log(paragraphEle);
